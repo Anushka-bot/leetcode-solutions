4d1631e644d98bd74a5a272e0260b0ae99d525ca
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-30 10:08:57 +0530

    Code

---
+++ problems/n-ary-tree-preorder-traversal.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct Node {
+    int val;
+    int numChildren;
+    struct Node** children;
+};
+
+void order(struct Node *root, int *arr, int *index){ 
+    if(root == NULL)
+        return;
+    arr[*index] = root->val;
+    *index = *index + 1;
+
+    for(int i = 0; i < root->numChildren; i++)
+        order(root->children[i], arr, index);
+}
+int* preorder(struct Node* root, int* returnSize) {
+    int index = 0;
+    int *resArr = (int *)malloc(10000 * sizeof(int));
+    order(root, resArr, &index);
+    *returnSize = index;
+    return resArr;
+}
+
+int main(){
+    
+    return 0;
+}
